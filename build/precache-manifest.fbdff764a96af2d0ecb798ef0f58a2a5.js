b91a52920a8a3e351b3959b0318575d7ff8383dd
Daniel Koublachvili <dannykoubz@gmail.com>
2020-11-02 18:42:02 +1100

    Changes for heroku

---
+++ build/precache-manifest.fbdff764a96af2d0ecb798ef0f58a2a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b30bc20e8350ba58110ab6e826c3435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffcb1008a2b1ebd5aaca",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2c107f20b676a1521e8f",
+    "url": "/static/css/main.bcd68488.chunk.css"
+  },
+  {
+    "revision": "ffcb1008a2b1ebd5aaca",
+    "url": "/static/js/2.b155eaf2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.b155eaf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c107f20b676a1521e8f",
+    "url": "/static/js/main.e380b970.chunk.js"
+  },
+  {
+    "revision": "e22c04dbef872ad9fd31",
+    "url": "/static/js/runtime-main.8dd0a6a8.js"
+  }
+]);
